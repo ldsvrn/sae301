6d9b77012e16b7f70f87294dc9431a44a516099e
ldsvrn <louis2555@orange.fr>
2022-10-21 10:37:04 +0200

    rebase

--- django/sae301/main/mqtt.py
+++ django/sae301/main/mqtt.py
@@ -1,4 +1,3 @@
-from email import message
 import paho.mqtt.client as mqtt
 import random
 import json
---
+++ plage-horaires/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import paho.mqtt.client as mqtt
+import random
+import json
+import datetime
