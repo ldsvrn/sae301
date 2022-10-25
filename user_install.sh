aae4104725faec53cfc59da974d1e7648888abbf
ldsvrn <louis2555@orange.fr>
2022-10-25 13:47:15 +0200

    scripts

--- user_install.sh
+++ user_install.sh
@@ -2,6 +2,7 @@
 
 python3 -m venv /home/toto/django/django/sae301/.venv
 source /home/toto/django/django/sae301/.venv/bin/activate
+pip3 install wheel
 pip3 install django django-admin gunicorn paho-mqtt
 
 python3 /home/toto/django/django/sae301/manage.py makemigrations
